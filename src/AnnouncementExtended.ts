0680dc4c62c94280e2bd7ea5558e9aae60d1f17e
denniskuhn <dennis@kuhn.geek.nz>
2021-10-08 16:51:01 +1300

    Remove initialisation

--- src/AnnouncementExtended.ts
+++ src/AnnouncementExtended.ts
@@ -1,22 +1,19 @@
-import { Link, Announcement, UserLookup } from '@mauriora/controller-sharepoint-list';
+import { Announcement, Link, UserLookup } from '@mauriora/controller-sharepoint-list';
 import { Expose, Type } from 'class-transformer';
 
-export class AnnouncementExtended extends Announcement {
-    public constructor() {
-        super();
-    }
 
+export class AnnouncementExtended extends Announcement {
     @Expose({ name: 'Urgent' })
-    public urgent: boolean = undefined;
+    public urgent: boolean;
 
     @Expose({ name: 'StartDate' })
-    public startDate: string = undefined;
+    public startDate: string;
 
     @Type( () => Link )
     @Expose({name: 'URL'})
-    public url: Link = undefined;
+    public url: Link;
 
     @Type( () => UserLookup )
     @Expose({name: 'ReportOwner'})
-    public contentOwner: UserLookup = undefined;
+    public contentOwner: UserLookup;
 }
